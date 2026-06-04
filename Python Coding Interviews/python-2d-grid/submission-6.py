e21510ad71bf1898c082cc546eabe163a3a4c6cf
prashan2 <prashanthchandrappa@gmail.com>
2026-06-04 22:13:52 +0530

    Add: python-2d-grid - submission-6

---
+++ Python Coding Interviews/python-2d-grid/submission-6.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def in_bounds(grid: List[List[int]], r: int, c: int) -> bool:
+    #print(f'r:{r} c:{c}')
+    for row_cnt,row in enumerate(grid):
+        for col_cnt, col in enumerate(row):
+            if c < col_cnt:
+                #print(f'col_cnt:{col_cnt} ')
+                return False
+        if r < row_cnt:
+            #print(f'row_cnt:{row_cnt} ')
+            return False
+    return True
+
+# do not modify below this line
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 0, 0))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2, 2))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, 1))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 4, 3))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, 4))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, -1))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], -1, 3))
